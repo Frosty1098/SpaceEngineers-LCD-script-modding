299f4c0b730d1a561626e9751eae03e2048f6ef8
Frosty1098 <43832395+Frosty1098@users.noreply.github.com>
2020-11-01 20:51:49 +1100

    Add files via upload

---
+++ TLSTestMod.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text;
+using VRage.Game.GUI.TextPanel;
+using VRageMath;
+using Sandbox.Game.GameSystems.TextSurfaceScripts;
+using Sandbox.ModAPI;
+using VRage.Game.ModAPI;
+
+namespace Frosty.CargoFillLcds
+{
+    [MyTextSurfaceScript("TSS_TEST", "TEST SCRIPT")]
+    public class MyTSSTestMod : MyTSSCommon
+    {
+        public static float ASPECT_RATIO = 2.5f;
+        public static float DECORATION_RATIO = 0.25f;
+        public static float TEXT_RATIO = 0.25f;
+        private StringBuilder m_sb = new StringBuilder();
+        private Vector2 m_innerSize;
+        private Vector2 m_decorationSize;
+        int num1 = 1;
+        public override ScriptUpdate NeedsUpdate
+        {
+            get
+            {
+                return ScriptUpdate.Update10;
+            }
+        }
+
+        public MyTSSTestMod(IMyTextSurface surface, IMyCubeBlock block, Vector2 size) : base(surface, block, size)
+        {
+            m_innerSize = new Vector2(ASPECT_RATIO, 1f);
+            FitRect(size, ref m_innerSize);
+            m_decorationSize = new Vector2(0.012f * m_innerSize.X, DECORATION_RATIO * this.m_innerSize.Y);
+            m_sb.Clear();
+            m_sb.Append("M");
+            Vector2 vector2 = surface.MeasureStringInPixels(m_sb, m_fontId, 1f);
+            m_fontScale = TEXT_RATIO * m_innerSize.Y / vector2.Y;
+        }
+
+        public override void Run()
+        {
+            using (MySpriteDrawFrame frame = m_surface.DrawFrame())
+            {
+                AddBackground(frame, new Color?(new Color(m_foregroundColor, 0.66f)));
+                num1 = num1 + 1;
+   
+                string str = "HELLO\n" + "MOD BY FROSTY\n" + num1.ToString();
+                Vector2 vector2 = m_surface.MeasureStringInPixels(new StringBuilder(str), m_fontId, m_fontScale);
+                MySprite sprite = new MySprite()
+                {
+                    Position = new Vector2?(new Vector2(m_halfSize.X, m_halfSize.Y - vector2.Y * 0.5f)),
+                    Size = new Vector2?(new Vector2(m_innerSize.X, m_innerSize.Y)),
+                    Type = SpriteType.TEXT,
+                    FontId = m_fontId,
+                    Alignment = TextAlignment.CENTER,
+                    Color = new Color?(m_foregroundColor),
+                    RotationOrScale = m_fontScale,
+                    Data = str
+                };
+                frame.Add(sprite);
+                float scale = (float)(m_innerSize.Y / 256.0 * 0.899999976158142);
+                //AddBrackets(frame, new Vector2(64f, 256f), scale);
+            }
+        }
+    }
+}
---
+++ txt to LCD test rectangle.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+using VRage.Game.GUI.TextPanel;
+using VRageMath;
+using Sandbox.Game.GameSystems.TextSurfaceScripts;
+using Sandbox.ModAPI;
+using VRage.Game.ModAPI;
+
+namespace Frosty.TXTLCDR
+{
+    [MyTextSurfaceScript("TXT LCD R", "TXT to LCD Rectangle")]
+    public class MyTSSTestMod : MyTSSCommon
+    {
+        public static float ASPECT_RATIO = 1f;
+        public static float DECORATION_RATIO = 0.25f;
+        public static float TEXT_RATIO = 0.05f;
+        private StringBuilder m_sb = new StringBuilder();
+        private Vector2 m_innerSize;
+        private Vector2 m_decorationSize;
+        public override ScriptUpdate NeedsUpdate
+        {
+            get
+            {
+                return ScriptUpdate.Update100;
+            }
+        }
+
+        public MyTSSTestMod(IMyTextSurface surface, IMyCubeBlock block, Vector2 size) : base(surface, block, size)
+        {
+            m_innerSize = new Vector2(ASPECT_RATIO, 1f);
+            FitRect(size, ref m_innerSize);
+            m_decorationSize = new Vector2(0.012f * m_innerSize.X, DECORATION_RATIO * this.m_innerSize.Y);
+            m_sb.Clear();
+            m_sb.Append("M");
+            Vector2 vector2 = surface.MeasureStringInPixels(m_sb, m_fontId, 1f);
+            m_fontScale = TEXT_RATIO * m_innerSize.Y / vector2.Y;
+        }
+		
+        public override void Run()
+        {
+            using (MySpriteDrawFrame frame = m_surface.DrawFrame())
+            {
+                AddBackground(frame, new Color?(new Color(m_foregroundColor, 0.66f)));
+                string str = "No file found";
+				if (MyAPIGateway.Utilities.FileExistsInGlobalStorage("OutputHere.txt")){
+				var read = MyAPIGateway.Utilities.ReadFileInGlobalStorage("OutputHere.txt");
+				str = read.ReadToEnd();
+				read.Dispose();
+				}
+				
+                Vector2 vector2 = m_surface.MeasureStringInPixels(new StringBuilder(str), m_fontId, m_fontScale);
+                MySprite sprite = new MySprite()
+                {
+                    Position = new Vector2?(new Vector2(0, m_halfSize.Y *1.6f - vector2.Y )),
+                    Size = new Vector2?(new Vector2(m_innerSize.X, m_innerSize.Y)),
+                    Type = SpriteType.TEXT,
+                    FontId = m_fontId,
+                    Alignment = TextAlignment.LEFT,
+                    Color = new Color?(m_foregroundColor),
+                    RotationOrScale = m_fontScale,
+                    Data = str
+                };
+                frame.Add(sprite);
+                float scale = (float)(m_innerSize.Y / 256.0 * 0.899999976158142);
+                //AddBrackets(frame, new Vector2(64f, 256f), scale);
+            }
+        }
+    }
+}
